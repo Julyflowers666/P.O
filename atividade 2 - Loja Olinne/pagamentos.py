9e12dca04dba1f831e4a83ba47c44823a80c9e6f
Julyflowers666 <jullya.leite.flores@gmail.com>
2026-07-22 21:06:58 -0400

    atulização

--- atividade 2 - Loja Olinne/pagamentos.py
+++ atividade 2 - Loja Olinne/pagamentos.py
@@ -3,3 +3,6 @@ class Pagamentos:
         self.pix = pix
         self.cartao = cartao
         self.vale = vale
+
+    def __str__(self):
+        return (f"metados de pagamentos: {self.pix} + {self.cartao} + {self.vale}") 
